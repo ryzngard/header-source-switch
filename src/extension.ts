26206f741052a42756367d3cf106bf9e53b1bdc5
ryzngard <ryzngard@live.com>
2016-05-04 13:51:37 -0700

    Add new ability to open in right pane
    Change the file search to be a promise, that returns a file name or
    null. Then, separate out the commands to have an "Open in right pane"
    behavior, which always opens in pane 2.

--- src/extension.ts
+++ src/extension.ts
@@ -34,78 +34,145 @@ function openFile(fileName:string) {
     }
 }
 
+function openFileInRightPane(fileName:string) {
+    console.log("Opening " + fileName + " in right pane");
+
+    let uriFile = vscode.Uri.file(fileName);
+    let work = vscode.workspace.openTextDocument(uriFile);
+    work.then(
+        document => {
+            console.log("Done opening " + document.fileName);
+            vscode.window.showTextDocument(document, vscode.ViewColumn.Two);
+        }
+    ).then(() => {return;}, (error) => { console.error(error); }) ;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.switch', () => {
-        let editor = vscode.window.activeTextEditor;
-        let document = editor.document.fileName;
-        let dir = path.dirname(document);
-        let extension = path.extname(document);
-        let fileWithoutExtension = path.basename(document).replace(extension, '');
-
-        if (extension.length == 0)
-        {
-            // Do nothing if there is no extension
-            return;
-        }
-
-
-
-        // Determine if the file is a header or source file.
-        let extensions : String[];
-
-        if (headerExtensions.indexOf(extension) != -1) {
-            extensions = sourceExtensions;
-        }
-        else
-        {
-            extensions = headerExtensions;
-        }
-
-        let extRegex = "(\\" + extensions.join("|\\") + ")$";
-        let newFileName = fileWithoutExtension;
-        let found : boolean = false;
-
-        // Search the current directory for a matching file
-        let filesInDir: String[] = fs.readdirSync(dir).filter((value: string, index: number, array: string[]) =>
-        {
-            return (path.extname(value).match(extRegex) != undefined);
-        });
-
-        for (var i = 0; i < filesInDir.length; i++)
-        {
-            let fileName: String = filesInDir[i];
-            let match = fileName.match(fileWithoutExtension + extRegex);
-            if (match)
+    var disposable = vscode.commands.registerCommand('extension.switch', async () => {
+        getHeaderSourceAsync().then(
+            (filename:string) =>
             {
-                found = true;
-                newFileName = match[0];
-                break;
+                if (filename != null)
+                {
+                    openFile(filename);
+                }
             }
-        }
-
-        if (found)
-        {
-            let newFile = path.join(dir, newFileName);
-            openFile(newFile);
-        }
-        else
-        {
-            extensions.forEach(ext => {
-                vscode.workspace.findFiles('**/'+fileWithoutExtension+ext, '', 1).then((fileURIs : vscode.Uri[]) =>
-                    {
-                        if (fileURIs.length > 0)
-                        {
-                            openFile(fileURIs[0].fsPath);
-                        }
-                    });
-            });
-        }
+        );
     });
 
     context.subscriptions.push(disposable);
+
+    var switchNewPaneDisposable = vscode.commands.registerCommand('extension.switchRightPane', async () => {
+        getHeaderSourceAsync().then(
+            (filename:string) =>
+            {
+                if (filename != null)
+                {
+                    openFileInRightPane(filename);
+                }
+            }
+        );
+    });
+
+    context.subscriptions.push(switchNewPaneDisposable);
+}
+
+function getHeaderSourceAsync() : Thenable<string>
+{
+    let editor = vscode.window.activeTextEditor;
+    let document = editor.document.fileName;
+    let dir = path.dirname(document);
+    let extension = path.extname(document);
+    let fileWithoutExtension = path.basename(document).replace(extension, '');
+
+    if (extension.length == 0)
+    {
+        // Do nothing if there is no extension
+        return;
+    }
+
+
+
+    // Determine if the file is a header or source file.
+    let extensions : String[];
+
+    if (headerExtensions.indexOf(extension) != -1) {
+        extensions = sourceExtensions;
+    }
+    else
+    {
+        extensions = headerExtensions;
+    }
+
+    let extRegex = "(\\" + extensions.join("|\\") + ")$";
+    let newFileName = fileWithoutExtension;
+    let found : boolean = false;
+
+    // Search the current directory for a matching file
+    let filesInDir: String[] = fs.readdirSync(dir).filter((value: string, index: number, array: string[]) =>
+    {
+        return (path.extname(value).match(extRegex) != undefined);
+    });
+
+    for (var i = 0; i < filesInDir.length; i++)
+    {
+        let fileName: String = filesInDir[i];
+        let match = fileName.match(fileWithoutExtension + extRegex);
+        if (match)
+        {
+            found = true;
+            newFileName = match[0];
+            break;
+        }
+    }
+
+    if (found)
+    {
+        let newFile = path.join(dir, newFileName);
+        return new Promise<string>((resolve, reject) => {
+            resolve(newFile);
+        });
+    }
+    else
+    {
+        return new Promise<string>((resolve, reject) =>
+        {
+            let promises = new Array<Promise<vscode.Uri[]>>();
+            extensions.forEach(ext => {
+                promises.push(new Promise<vscode.Uri[]>(
+                    (resolve, reject) =>
+                    {
+                        vscode.workspace.findFiles('**/'+fileWithoutExtension+ext, '', 1).then(
+                            (uris) =>
+                            {
+                                resolve(uris);
+                            }
+                        );
+                    }));
+            });
+
+            Promise.all(promises).then(
+                (values:any[]) => {
+                    let resolved = false;
+                    values.forEach(p => {
+                       if (p.length > 0)
+                       {
+                           resolved = true;
+                           resolve(path.normalize(p[0].fsPath));
+                       }
+                    });
+
+                    if (!resolved)
+                    {
+                        resolve(null);
+                    }
+                }
+            );
+        });
+    }
 }
